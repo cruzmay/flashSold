d3dbdb97d2a5955977f9c9c0dfc4e94bacd47f24
Pedro Cruz <pcruzmay@yahoo.com>
2020-11-26 18:47:48 -0600

    pagina funcionando

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Esta es una pagina que se creo con le fin de cotizar los viajes para envios locales.
+Esta es una pagina que se creo con el fin de cotizar los viajes para envios locales.
--- src/components/NewMap.jsx
+++ src/components/NewMap.jsx
@@ -8,12 +8,22 @@ import {
 import mapStyles from '../assets/styles/mapStyles'
 import '../assets/styles/Header.scss'
 import Logo from '../components/Logo'
-
+import usePlacesAutoComplete, 
+{getGeocode, 
+getLatLng,
+getDetails
+} from 'use-places-autocomplete';
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxOption
+} from '@reach/combobox'
+import "@reach/combobox/styles.css";
 
 
 
 const NewMap = () => {
-
 
     const [directions, setDirections] = useState();
     const [queryOrigin, setQueryOrigin] = useState(""); 
@@ -22,18 +32,21 @@ const NewMap = () => {
     console.log('Destination:',queryDestination)
     const count = useRef(0);
 
+    const libraries = [
+        'places',
+        'geometry',
+    ]
+        
     const mapContainerStyle = {
         height: '100vh',
         width: '100%',
     }
-    const origin = {
-        lat: 20.759840,
-        lng: -103.387489
-    };
-    const destination = {
-        lat: 20.548180,
-        lng: -103.313591
-    }
+    
+    
+    
+    // console.log('vienes de input', originMark)
+    const origin = queryOrigin || {lat: 20.548180, lng: -103.313591  } ;
+    const destination = queryDestination || {lat: 20.548180, lng: -103.313591 };
 
     const options = {
     styles: mapStyles,
@@ -56,26 +69,32 @@ const NewMap = () => {
     }
   };
 
-
+//   const PanToOrigin = React.useCallback(({ lat, lng }) => {
+//     mapRef.current.PanToOrigin({lat, lng})
+//     map
+//   })
 
     return (
         <>
+        <LoadScript googleMapsApiKey='AIzaSyAzhghT0R8MfIl7CsOCBtDgOWn_BJvJDPQ' libraries={libraries}>
+
         <header className="map__header">
-        <div className="map__header__promo">
-            <h3>VENDE LO QUE QUIERAS NOSOTROS LO LLEVAMOS - 
-                 <span> TU MEDIO ECÓNOMICO DE ENVIOS LOCALES</span>
-            </h3>
-        </div>
-        <nav className="map__header__nav">
-            <figure>
-                <Logo/>
-            </figure>
-            <form action="post">
-                <input 
+            <div className="map__header__promo">
+                <h3>VENDE LO QUE QUIERAS NOSOTROS LO LLEVAMOS - 
+                    <span> TU MEDIO ECÓNOMICO DE ENVIOS LOCALES</span>
+                </h3>
+            </div>
+            <nav className="map__header__nav">
+                <figure>
+                    <Logo/> 
+                </figure>
+                <form action="post">
+                    {/* <Search/> */}
+                     <input 
                     type="search" 
                     placeholder="ENVIA DE:"
                     value={queryOrigin}
-                    onChange={(e) => {
+                    onInput={ (e) => {
                     setQueryOrigin(e.target.value);
                     }}
                 />
@@ -83,18 +102,14 @@ const NewMap = () => {
                     type="search" 
                     placeholder="A:"
                     value={queryDestination}
-                    onChange={(e) => {
+                    onInput={ (e) => {
                     setQueryDestination(e.target.value);
-                }}
-                
-                />
-                
-            </form>    
-        </nav>
-    </header>
-
-        <LoadScript googleMapsApiKey='AIzaSyAzhghT0R8MfIl7CsOCBtDgOWn_BJvJDPQ'>
-            <GoogleMap
+                }} 
+                 />
+                </form>   
+            </nav>
+        </header>
+        <GoogleMap
                 mapContainerStyle = {mapContainerStyle}
                 zoom={12}
                 center={origin} 
@@ -106,16 +121,53 @@ const NewMap = () => {
                         destination: destination,
                         travelMode: 'DRIVING'
                     }}
-                    callback = {directionsCallback }
+                    callback = {directionsCallback}
                 />
                 <DirectionsRenderer
                     directions = {directions}
                 />
                 {/* <Marker position={origin} /> */}
-            </GoogleMap>
-        </LoadScript>
+        </GoogleMap>
+    </LoadScript>
         </>
     )
 }
+
+// const Search = () => {
+//     const {ready, 
+//         value, 
+//         suggestions: 
+//         {status, data}, 
+//         setValue, 
+//         clearSuggestions} = usePlacesAutoComplete({
+//         requestOptions: {
+//            location: {lat: () => 20.759840, lng: () => -103.387489 },
+//            radius: 200 * 1000,
+//         }
+//     });
+//     return <Combobox onSelect={ async (address)=>{
+//         try {
+//             const results = await getGeocode({address});
+//             const { lat, lng } = await getLatLng(results[0])
+//             const originMark = {lat, lng}
+//             console.log(originMark)
+//         } catch(error) {
+//             console.log('error')
+//         }
+//     }}>
+//         <ComboboxInput 
+//             value={value} onChange={(e)=>{
+//                 setValue(e.target.value)
+//             }}
+//             disabled={!ready}
+//             placeholder = "DE:"
+//         />
+//         <ComboboxPopover>
+//             {status === "OK" && data.map(({id, description})=> 
+//                 <ComboboxOption key={id} value={description} />
+//             )}
+//         </ComboboxPopover>
+//     </Combobox>
+// }
 
 export default NewMap;
